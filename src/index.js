48746215714f197e7f56e34b566b9a9679c81c08
artmuley87 <41024852+artmuley87@users.noreply.github.com>
2018-07-10 16:55:45 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Greeting} from "./greeting.js";
+import "./styles.css"
+
+class Body extends React.Component {
+  getname(){
+    var name;
+    //name = prompt( "Enter your name","");
+    return name;
+  }
+  render(){
+    return(
+      <div>
+        <Greeting name={this.getname()} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Body />, document.getElementById('root'));
